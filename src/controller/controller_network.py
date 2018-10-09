c78f77eaf887f01c815d02b95ea8dae6c06f2339
david <david@student-net-cx-2035.ethz.ch>
2018-10-09 16:21:35 +0200

    Should be able to generate the dag now (the general format at least)

--- src/controller/controller_network.py
+++ src/controller/controller_network.py
@@ -13,49 +13,34 @@ class ControllerLSTM(nn.Module):
 
         super(ControllerLSTM, self).__init__()
 
-        # Initialize the embedding (from the tokens)
-        self.embedding_block = nn.Embedding(
-            4,  # TODO: change this to something appropriate
-            ARG.controller_hid
-        )  # The embedding which specifies the block to which this is connected to
-        self.embedding_activation = nn.Embedding(
-            ARG.num_blocks,
-            ARG.controller_hid
-        )
-        # TODO: don't quite understand what this embedding is doing
-
-        # Initialize the decoding (to the tokens)
-
         # Initialize the internal cell used for this controller LSTM
         self.lstm = torch.nn.LSTMCell(
             ARG.controller_hid,
             ARG.controller_hid
         )
 
-        # We have an individual decoder for
-        # each individual token
-
         # Simply have one encoder for each individual sequence item
-        self.encoders = {}
+        self.encoders_activation = {}
+        self.encoders_block = {}
         # We don't have an encoder for the very first item!
         for block_idx in range(1, ARG.num_blocks):
-
             # Embedding from activation function to encoder
-            self.encoders[2*block_idx-1] = nn.Embedding(
+            self.encoders_activation[block_idx] = nn.Embedding(
                 4,
                 ARG.controller_hid
             )
 
-            # Embedding from previous block to the encoder
-            self.econders[2*block_idx] = nn.Embedding(
+            self.encoders_block[block_idx] = nn.Embedding(
                 block_idx,
                 ARG.controller_hid
             )
 
-        self.decoders = {}
+        # Now comes the decoder functions
         # The first decoder is a linear connection to the activation
+        self.decoders_activation = {}
+        self.decoders_block = {}
         # (this should be shared actually?)
-        self.decoders[0] = nn.Linear(
+        self.decoders_activation[0] = nn.Linear(
             ARG.controller_hid,
             4,
             bias=False
@@ -65,14 +50,14 @@ class ControllerLSTM(nn.Module):
         for block_idx in range(1, ARG.num_blocks):
 
             # Decoder from the hidden state to the activation function
-            self.decoders[2*block_idx-1] = torch.nn.Linear(
+            self.decoders_activation[block_idx] = torch.nn.Linear(
                 ARG.controller_hid,
                 4,
                 bias=False
             )
 
             # Decoder from the hdiden state to the block deciding function
-            self.decoders[2*block_idx] = nn.Linear(
+            self.decoders_block[block_idx] = nn.Linear(
                 ARG.controller_hid,
                 block_idx,
                 bias=False
@@ -103,23 +88,70 @@ class ControllerLSTM(nn.Module):
 
         # The hidden state consists of two items
         self.initial_cell = torch.nn.Parameter(
-            torch.randn((ARG.controller_batch_size, ARG.controller_hid,)),
+            torch.zeros(
+                (ARG.controller_batch_size, ARG.controller_hid,)
+            ),
             requires_grad=False
         )
         self.initial_hidden = torch.nn.Parameter(
-            torch.randn((ARG.controller_batch_size, ARG.controller_hid,)),
+            torch.zeros(
+                (ARG.controller_batch_size, ARG.controller_hid,)
+            ),
             requires_grad=False
         )
         self.initial_input = torch.nn.Parameter(
-            torch.randn((ARG.controller_batch_size, ARG.controller_hid)),
+            torch.zeros(
+                (ARG.controller_batch_size, ARG.controller_hid)
+            ),
             requires_grad=False
         )
 
-        torch.nn.init.zeros_(self.initial_cell)
-        torch.nn.init.zeros_(self.initial_hidden)
-        torch.nn.init.zeros_(self.initial_input)
+    def forward_activation(self, inputs, hidden, cell, block_id):
+        """
+            Do one forward pass with the sampler to get an
+            activation function
+        :param inputs:
+        :param hidden:
+        :param cell:
+        :param block_id:
+        :return:
+        """
+        # Encode (if not block_id == 0)
+        if block_id == 0:
+            embedded = inputs
+        else:
+            embedded = self.encoders_block[block_id](inputs)
 
-    def forward(self, inputs, hidden, block_id, is_embedded):
+        # Run through LSTM
+        new_hidden, new_cell = self.lstm(embedded, (hidden, cell))
+
+        # Decode
+        logits = self.decoders_activation[block_id](new_hidden)
+
+        return logits, new_hidden, new_cell
+
+    def forward_block(self, inputs, hidden, cell, block_id):
+        """
+            Do one forward pass with the sampler to get a
+            previous block to connect to
+        :param inputs:
+        :param hidden:
+        :param cell:
+        :param block_id:
+        :return:
+        """
+        # Is always embedded
+        embedded = self.encoders_activation[block_id](inputs)
+
+        # Run through LSTM
+        new_hidden, new_cell = self.lstm(embedded, (hidden, cell))
+
+        # Decode
+        logits = self.decoders_block[block_id](new_hidden)
+
+        return logits, new_hidden, new_cell
+
+    def forward(self, inputs):
         """
             Do one forward pass with the sampler.
             Apply the forward and apply a decoder
@@ -128,84 +160,44 @@ class ControllerLSTM(nn.Module):
         :return:
         """
 
-        # Depending on is_embedding,
-        # we take the raw inputs vs not the raw inputs
-        if not is_embedded:
-            # embed = self.encoder(inputs)
-            embed = inputs
-        else:
-            embed = inputs
-
-        # Pass through the lstm
-        new_hidden, new_cell = self.lstm(embed, hidden)
-        # logits = self.decoders[block_id](new_hidden)  # So we have different decoders for each block!
-        logits = new_hidden
-
-        return logits, (new_hidden, new_cell)
-
-    # Sampling logic
-    # def _sample_activation(self, input, hidden_h, hidden_c):
-    #     logits, hidden, cell = self.lstm(input, (hidden_h, hidden_c))
-    #
-    #     return logits, hidden, cell
-    # def _sample_previous_layer(self, input, hidden_h, hidden_c):
-    #     logits, hidden, cell = self.lstm(input, (hidden_h, hidden_c))
-    #
-    #     # Pass the output through the embedding that samples the previous activation layer
-    #     return logits, hidden, cell
-
-    def sample(self, batch_size=1, verbose=True):
-        """
-            Repeatedly run the forward pass,
-            until the corresponding string has been sampled.
-            This function makes use of the following functions to handle the logic:
-                - _sample_activation
-                - _sample_previous_layer
-
-            We iteratively first sample the activation function,
-            and then the previous block id
-
-        :return:
-        """
-
-        # Reset all outputs and inputs
+        # Have arrays for individual actions (to generate the string from this later)
         activations = []
-        prev_nodes = []
+        previous_blocks = []
 
+        # Inputs and hidden should be sampled from zero
         self._reset_initial_states()
 
         inputs = self.initial_input
         hidden = self.initial_hidden
         cell = self.initial_cell
 
-        for block_idx in range(2 * (ARG.num_blocks - 1) + 1):
-            print("Going through the following idx: ", block_idx, ARG.num_blocks)
-            print("Shapes: ", inputs.size())
-            print("Shapes hidden: ", hidden.size())
-            print("Shapes cell: ", cell.size())
+        # Do one pass through the first activation
+        print("Outside initial cell")
+        inputs, hidden, cell = self.forward_activation(inputs, hidden, cell, 0)
+        inputs = torch.argmax(inputs, dim=1, keepdim=False)
 
-            logits, (hidden, cell) = self.forward(
-                inputs=inputs,
-                hidden=(hidden, cell),
-                block_id=block_idx,  # This seems terribly wrong!
-                is_embedded=(block_idx == 0)
-            )
+        for block_id in range(1, ARG.num_blocks):
 
-            print("Logits are: ", logits.size())
+            print("Shape of input is: ", inputs.shape)
+            print(inputs)
 
-        # Initial element gives the activation for the activation function
+            print("Block idx is: (block input) ", block_id)
+            # First get the previous layer
+            inputs, hidden, cell = self.forward_block(inputs, hidden, cell, block_id)
 
-        # First sample the
+            # Need to pass logits through the embedding, so take argmax, right?
+            inputs = torch.argmax(inputs, dim=1, keepdim=False)
 
-    # def forward(self, inputs, hidden=None, force=True, steps=0):
-    #
-    #     outputs = Variable(torch.zeros(steps, 1, 1))
-    #     output = None
-    #
-    #     # Do the actual forward pass
-    #     for i in range(steps):
-    #         if force or i == 0 or (output is None):
-    #             input = inputs[i
+            print("Block idx is: (activation input) ", block_id)
+            # Second get the activation
+            inputs, hidden, cell = self.forward_activation(inputs, hidden, cell, block_id)
+
+            # Need to pass logits through the embedding again
+            inputs = torch.argmax(inputs, dim=1, keepdim=False)
+
+
+        return inputs, hidden, cell
+
 
     # def reset_parameters(self):
     #     init_range = 0.1
@@ -219,6 +211,5 @@ if __name__ == "__main__":
     print("Starting the controller lstm")
     controller = ControllerLSTM()
 
-    controller.sample()
+    controller.forward(None)
 
-    print(controller.decoders)
